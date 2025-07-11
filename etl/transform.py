48565d7a1a7ae3e685738bc5a640dedf17653a9b
Nataya Flores <natayafs@gmail.com>
2025-07-11 13:03:03 -0300

    Nuevo feature de duplicados

--- etl/transform.py
+++ etl/transform.py
@@ -3,5 +3,5 @@ import pandas as pd
 def transform(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = [col.lower().strip() for col in df.columns]
     df = df.dropna()
-    # df = df.drop_duplicates()
+    df = df.drop_duplicates()
     return df
